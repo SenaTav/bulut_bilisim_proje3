2c1ce38c441ba1fe996ff42e30b9ce4491307d7d
SenaTav <senatavv@gmail.com>
2026-06-14 10:22:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/tahmin'
+url = 'http://18.212.121.16:5000/tahmin'
 veri = {
     "ozellikler": [8.3252, 41.0, 6.98, 1.02, 322.0, 2.55, 37.88, -122.23]
 }
